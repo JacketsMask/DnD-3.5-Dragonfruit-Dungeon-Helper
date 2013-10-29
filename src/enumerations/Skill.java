482c23c44dd7597ab1155ea7447a7857dcb1f83b
Japhez <JaphezBeMeYe@hotmail.com>
2013-10-29 15:27:28 -0400

    Added method to retrieve all skills.

--- src/enumerations/Skill.java
+++ src/enumerations/Skill.java
@@ -17,6 +17,18 @@ public enum Skill {
     SPOT, SURVIVAL, SWIM, TUMBLE, USE_MAGIC_DEVICE, USE_PSIONIC_DEVICE, USE_ROPE;
 
     /**
+     * @return an array of all skills
+     */
+    public static Skill[] getAllSkills() {
+        return new Skill[]{APPRAISE, AUTOHYPNOSIS, BALANCE, BLUFF, CLIMB, CONCENTRATION, CONTROL_SHAPE,
+            CRAFT, DECIPHER_SCRIPT, DIPLOMACY, DISABLE_DEVICE, DISGUISE, ESCAPE_ARTIST,
+            FORGERY, GATHER_INFORMATION, HANDLE_ANIMAL, HEAL, HIDE, INTIMIDATE, JUMP,
+            KNOWLEDGE, LISTEN, MOVE_SILENTLY, OPEN_LOCK, PERFORM, PSICRAFT, PROFESSION,
+            RIDE, SEARCH, SENSE_MOTIVE, SLEIGHT_OF_HAND, SPEAK_LANGUAGE, SPELLCRAFT,
+            SPOT, SURVIVAL, SWIM, TUMBLE, USE_MAGIC_DEVICE, USE_PSIONIC_DEVICE, USE_ROPE};
+    }
+
+    /**
      * @return an array of strength based skills
      */
     public static Skill[] getStrSkills() {
