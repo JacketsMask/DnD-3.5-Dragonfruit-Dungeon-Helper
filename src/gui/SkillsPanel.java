94a5b8d50bf5613b95f6363004ff251738a537c9
Japhez <JaphezBeMeYe@hotmail.com>
2013-10-30 17:00:40 -0400

    Updated to use sweet new FilteredListModel.

--- src/gui/SkillsPanel.java
+++ src/gui/SkillsPanel.java
@@ -4,7 +4,7 @@ import character.Player;
 import enumerations.Skill;
 import interfaces.CharacterInfoRetriever;
 import java.util.ArrayList;
-import main.FilterTextField;
+import main.FilteredListModel;
 
 /**
  *
@@ -13,6 +13,7 @@ import main.FilterTextField;
 public class SkillsPanel extends javax.swing.JPanel implements CharacterInfoRetriever {
 
     private Player player;
+    private FilteredListModel skillListModel;
 
     /**
      * Creates new form SkillsPanel
@@ -20,19 +21,17 @@ public class SkillsPanel extends javax.swing.JPanel implements CharacterInfoRetr
     public SkillsPanel(Player player) {
         this.player = player;
         initComponents();
-        initSkillFilterTextField();
+        skillListModel = new FilteredListModel(skillFilterTextField);
+        skillList.setModel(skillListModel);
+        fillSkillModel();
     }
 
-    private void initSkillFilterTextField() {
-        FilterTextField<Skill> filter = (FilterTextField<Skill>) skillFilterSearchableTextField;
-        //debug, using all skills instead of player skills
-        ArrayList<Skill> arrayList = new ArrayList();
+    private void fillSkillModel() {
+        ArrayList data = skillListModel.getData();
         for (Skill s : Skill.getAllSkills()) {
-            arrayList.add(0,s);
+            data.add(0, s);
         }
-        filter.setConnectedList(skillList);
-        filter.setData(arrayList);
-        skillList.setVisible(true);
+        skillListModel.setData(data);
     }
 
     /**
@@ -46,12 +45,14 @@ public class SkillsPanel extends javax.swing.JPanel implements CharacterInfoRetr
 
         scrollPane = new javax.swing.JScrollPane();
         skillList = new javax.swing.JList();
-        skillFilterSearchableTextField = new FilterTextField();
+        skillFilterTextField = new javax.swing.JTextField();
         rollButton = new javax.swing.JButton();
         skillModifierTextField = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         showSkillModifiersButton = new javax.swing.JButton();
+        tableScrollPane = new javax.swing.JScrollPane();
+        skillTable = new javax.swing.JTable();
 
         skillList.setModel(new javax.swing.AbstractListModel() {
             String[] strings = { "sup" };
@@ -73,6 +74,55 @@ public class SkillsPanel extends javax.swing.JPanel implements CharacterInfoRetr
             }
         });
 
+        skillTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "Skill", "Value"
+            }
+        ));
+        tableScrollPane.setViewportView(skillTable);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -82,17 +132,22 @@ public class SkillsPanel extends javax.swing.JPanel implements CharacterInfoRetr
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(scrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(showSkillModifiersButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(rollButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(skillModifierTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addComponent(skillFilterSearchableTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(31, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(scrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                        .addComponent(jLabel1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(skillModifierTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addComponent(showSkillModifiersButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(rollButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 176, Short.MAX_VALUE))))
+                            .addComponent(skillFilterTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(tableScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -100,20 +155,22 @@ public class SkillsPanel extends javax.swing.JPanel implements CharacterInfoRetr
                 .addContainerGap()
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(skillFilterSearchableTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(skillModifierTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(skillFilterTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(rollButton)
-                        .addGap(18, 18, 18)
-                        .addComponent(showSkillModifiersButton)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE))
-                .addContainerGap())
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(jLabel1)
+                                    .addComponent(skillModifierTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addComponent(rollButton)
+                                .addGap(18, 18, 18)
+                                .addComponent(showSkillModifiersButton))
+                            .addComponent(scrollPane)))
+                    .addComponent(tableScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -126,9 +183,11 @@ public class SkillsPanel extends javax.swing.JPanel implements CharacterInfoRetr
     private javax.swing.JButton rollButton;
     private javax.swing.JScrollPane scrollPane;
     private javax.swing.JButton showSkillModifiersButton;
-    private javax.swing.JTextField skillFilterSearchableTextField;
+    private javax.swing.JTextField skillFilterTextField;
     private javax.swing.JList skillList;
     private javax.swing.JTextField skillModifierTextField;
+    private javax.swing.JTable skillTable;
+    private javax.swing.JScrollPane tableScrollPane;
     // End of variables declaration//GEN-END:variables
 
     @Override
