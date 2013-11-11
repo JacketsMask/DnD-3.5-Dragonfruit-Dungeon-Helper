b0bb6e29a067e6486b0d255fd77ca9f539d43f9c
Japhez <JaphezBeMeYe@hotmail.com>
2013-11-11 17:28:18 -0500

    Added method to retrieve all weapon proficiencies.

--- src/character/proficiencies/WeaponProficiency.java
+++ src/character/proficiencies/WeaponProficiency.java
@@ -26,6 +26,83 @@ public enum WeaponProficiency {
     ORC_DOUBLE_AXE, REPEATING_HEAVY_CROSSBOW, REPEATING_LIGHT_CROSSBOW, SAI,
     SHURIKEN, SIANGHAM, SPIKED_CHAIN, TWO_BLADED_SWORD, WHIP;
 
+    public static ArrayList<WeaponProficiency> getAllProficiencies() {
+        ArrayList<WeaponProficiency> list = new ArrayList<>();
+        list.add(CLUB);
+        list.add(DAGGER);
+        list.add(DART);
+        list.add(GAUNTLET);
+        list.add(HEAVY_CROSSBOW);
+        list.add(HEAVY_MACE);
+        list.add(JAVELIN);
+        list.add(LIGHT_CROSSBOW);
+        list.add(LIGHT_MACE);
+        list.add(LONGSPEAR);
+        list.add(MORNINGSTAR);
+        list.add(PUNCHING_DAGGER);
+        list.add(QUARTERSTAFF);
+        list.add(SHORTSPEAR);
+        list.add(SICKLE);
+        list.add(SLING);
+        list.add(SPEAR);
+        list.add(SPIKED_GAUNTLET);
+        list.add(UNARMED_STRIKE);
+        list.add(ARMOR_SPIKES);
+        list.add(BATTLEAXE);
+        list.add(COMPOSITE_LONGBOW);
+        list.add(COMPOSITE_SHORTBOW);
+        list.add(FALCHION);
+        list.add(FLAIL);
+        list.add(GLAIVE);
+        list.add(GREATAXE);
+        list.add(GREATCLUB);
+        list.add(GREATSWORD);
+        list.add(GUISARME);
+        list.add(HALBERD);
+        list.add(HANDAXE);
+        list.add(HEAVY_FLAIL);
+        list.add(HEAVY_PICK);
+        list.add(HEAVY_SHIELD);
+        list.add(KUKRI);
+        list.add(LANCE);
+        list.add(LIGHT_HAMMER);
+        list.add(LIGHT_PICK);
+        list.add(LIGHT_SHIELD);
+        list.add(LONGBOW);
+        list.add(LONGSWORD);
+        list.add(RANSEUR);
+        list.add(RAPIER);
+        list.add(SAP);
+        list.add(SCIMITAR);
+        list.add(SCYTHE);
+        list.add(SHORT_SWORD);
+        list.add(SHORTBOW);
+        list.add(THROWING_AXE);
+        list.add(TRIDENT);
+        list.add(WARHAMMER);
+        list.add(BASTARD_SWORD);
+        list.add(BOLAS);
+        list.add(DIRE_FLAIL);
+        list.add(DWARVEN_URGROSH);
+        list.add(DWARVEN_WARAXE);
+        list.add(GNOME_HOOKED_HAMMER);
+        list.add(HAND_CROSSBOW);
+        list.add(KAMA);
+        list.add(NET);
+        list.add(NET_OF_SNARING);
+        list.add(NUNCHAKU);
+        list.add(ORC_DOUBLE_AXE);
+        list.add(REPEATING_HEAVY_CROSSBOW);
+        list.add(REPEATING_LIGHT_CROSSBOW);
+        list.add(SAI);
+        list.add(SHURIKEN);
+        list.add(SIANGHAM);
+        list.add(SPIKED_CHAIN);
+        list.add(TWO_BLADED_SWORD);
+        list.add(WHIP);
+        return list;
+    }
+
     /**
      * @return all exotic weapon proficiencies in an ArrayList
      */
@@ -125,6 +202,7 @@ public enum WeaponProficiency {
     /**
      * A better toString method that returns a good looking string instead of a
      * enum looking one.
+     *
      * @return a better looking string
      */
     @Override
