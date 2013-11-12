ce25fb1bae589c8a1bf59fbdfbc1853af830a571
Japhez <JaphezBeMeYe@hotmail.com>
2013-11-12 15:19:39 -0500

    Added method to retrieve all alignments at once.

--- src/enumerations/Alignment.java
+++ src/enumerations/Alignment.java
@@ -1,5 +1,7 @@
 package enumerations;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jacob Dorman
@@ -7,6 +9,20 @@ package enumerations;
 public enum Alignment {
 
     LAWFUL_GOOD, NEUTRAL_GOOD, CHAOTIC_GOOD, LAWFUL_NEUTRAL, TRUE_NEUTRAL, CHAOTIC_NEUTRAL, LAWFUL_EVIL, NEUTRAL_EVIL, CHAOTIC_EVIL;
+    
+    public static ArrayList<Alignment> getAllAlignments() {
+        ArrayList<Alignment> arrayList = new ArrayList<>();
+        arrayList.add(LAWFUL_GOOD);
+        arrayList.add(NEUTRAL_GOOD);
+        arrayList.add(CHAOTIC_GOOD);
+        arrayList.add(LAWFUL_NEUTRAL);
+        arrayList.add(TRUE_NEUTRAL);
+        arrayList.add(CHAOTIC_NEUTRAL);
+        arrayList.add(LAWFUL_EVIL);
+        arrayList.add(NEUTRAL_EVIL);
+        arrayList.add(CHAOTIC_EVIL);
+        return arrayList;
+    }
 
     /**
      * Override for toString that returns the enumeration with the first letter
