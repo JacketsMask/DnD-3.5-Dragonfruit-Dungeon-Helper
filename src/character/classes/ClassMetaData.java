8e7b06ed39506d889d0f6f1139babb1c76cef602
Japhez <JaphezBeMeYe@hotmail.com>
2013-11-27 17:03:08 -0500

    Added class name to stored information.  Created methods to add and remove spells.

--- src/character/classes/ClassMetaData.java
+++ src/character/classes/ClassMetaData.java
@@ -1,21 +1,39 @@
 package character.classes;
 
+import character.Spell;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Contains meta data about a class, such a known spells and the class level.
+ * Contains meta data about a class, such as its name, known spells and the
+ * class level.
  *
  * @author Japhez
  */
 public class ClassMetaData implements Serializable {
 
+    //The name of the class
+    private String className;
+    //The current level of advancement within the class
     private int classLevel;
     //Class level: spell name
     private HashMap<Integer, ArrayList<String>> knownSpells;
 
-    public ClassMetaData() {
+    public ClassMetaData(String className) {
+        this.className = className;
+    }
+    
+    public void learnSpell(Spell spell) {
+        knownSpells.get(spell.getLevel()).add(spell.getName());
+    }
+    
+    public void unlearnSpell(Spell spell) {
+        knownSpells.get(spell.getLevel()).remove(spell.getName());
+    }
+
+    public String getClassName() {
+        return className;
     }
 
     public int getClassLevel() {
