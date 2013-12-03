daa7dcb923096ede79e80532364c1be289e3923f
Japhez <JaphezBeMeYe@hotmail.com>
2013-12-03 15:00:05 -0500

    Moved classes related to save state to their own package.

---
+++ src/savestate/SaveStateSender.java
@@ -0,0 +1,36 @@
+package savestate;
+
+/**
+ * This class holds data about a mutable state.
+ *
+ * This class should be extended by a data-storing class if GUI elements rely on
+ * the data in this class, or if the class needs to be checked by the
+ * SerializationThread.
+ *
+ * This class hooks into the DataRetrievalManager class to allow GUI elements to
+ * quickly update their data when necessary.
+ *
+ * @author Japhez
+ */
+public abstract class SaveStateSender {
+
+    private boolean stateChanged;
+
+    public SaveStateSender() {
+        stateChanged = false;
+    }
+
+    public void stateChanged() {
+        stateChanged = true;
+        //Signal that this sender's data has changed
+        DataRetrievalManager.dataChanged(this);
+    }
+
+    public boolean isStateChanged() {
+        return stateChanged;
+    }
+
+    public void stateSaved() {
+        stateChanged = false;
+    }
+}
